a9a9a09b4bc403bd9191ed7d7f035348ae6b11a8
MaMazav <idanigra@hotmail.com>
2016-06-13 00:32:08 +0300

    Simple export bug fix

--- src/resourceschedulerexports.js
+++ src/resourceschedulerexports.js
@@ -9,7 +9,7 @@ PriorityScheduler.prototype['enqueueJob'] = PriorityScheduler.prototype.enqueueJ
 PriorityScheduler.prototype['tryYield'] = PriorityScheduler.prototype.tryYield;
 PriorityScheduler.prototype['jobDone'] = PriorityScheduler.prototype.jobDone;
 
-LifoScheduler.prototype['shouldYieldOrAbort'] = PriorityScheduler.prototype.shouldYieldOrAbort;
+LifoScheduler.prototype['shouldYieldOrAbort'] = LifoScheduler.prototype.shouldYieldOrAbort;
 LifoScheduler.prototype['enqueueJob'] = LifoScheduler.prototype.enqueueJob;
 LifoScheduler.prototype['tryYield'] = LifoScheduler.prototype.tryYield;
 LifoScheduler.prototype['jobDone'] = LifoScheduler.prototype.jobDone;
